4c714a39c919679f9c290a02d39401bbbd253d9f
Ranidhumma <ranidhumma8@gmail.com>
2022-02-15 20:24:59 +0530

    calculate Wage using switch case

--- src/com/javaa/Employee.java
+++ src/com/javaa/Employee.java
@@ -2,29 +2,42 @@ package com.javaa;
 
 public class Employee {
 
-    public static void main(String[] args)
-			{
-					// constants
-					System.out.println("lets Add parttime calculate wage");
-					
-					int IS_FULL_TIME = 2;
-					int IS_PART_TIME = 1;
-					int EMP_RATE_PER_HR = 20;
-					//Variables
-							int empHrs = 0;
-							int empWage = 0;
-							// Computation
-					double empcheck = Math.floor(Math.random() * 10)% 3;
-					
-					if (empcheck == IS_FULL_TIME)
-						empHrs = 8;
-					
-					//	System.out.println("Employee is Present");
-					else if (empcheck == IS_PART_TIME)
-						//System.out.println("Employee is Absent");
-						empHrs = 4;
-					empWage = empHrs * EMP_RATE_PER_HR ;
-					System.out.println("Emp Wage: "  + empWage);
-				
-			}
-			}
+	
+	public static final int IS_FULL_TIME = 2;
+	public static final int IS_PART_TIME = 1;
+	public static final int EMP_RATE_PER_HR = 20;
+	
+	public static void main(String[] args)
+	{
+		//System.out.println("lets calculate using switch case");
+		System.out.println("Solving Using Switch Case statment");
+		
+	//Variables
+			int empHrs = 0;
+			int empWage = 0;
+			// Computation
+	double empcheck = Math.floor(Math.random() * 10)% 3;
+	switch ((int)empcheck) {
+	
+		case IS_FULL_TIME :
+			empHrs = 16;
+			break;
+		case IS_PART_TIME :
+			empHrs = 8;
+			break;
+		default:
+			empHrs = 0;
+		}
+
+	//if (empcheck == IS_FULL_TIME)
+		//empHrs = 16;
+	
+	//	System.out.println("Employee is Present");
+	//else if (empcheck == IS_PART_TIME)
+		//System.out.println("Employee is Absent");
+		//empHrs = 8;
+	empWage = empHrs * EMP_RATE_PER_HR ;
+	System.out.println("Emp Wage: "  + empWage);
+
+}
+}
