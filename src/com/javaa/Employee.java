55ddb939d79029a9d2091a6f617cebdc72ddf4f7
Ranidhumma <ranidhumma8@gmail.com>
2022-02-15 19:55:36 +0530

    calculated wage of emp

--- src/com/javaa/Employee.java
+++ src/com/javaa/Employee.java
@@ -3,15 +3,27 @@ package com.javaa;
 public class Employee {
 
  public static void main(String[] args)
-		{
-				int IS_FULL_TIME = 1;
-				double empcheck = Math.floor(Math.random() * 10)% 2;
-				
-				if (empcheck == IS_FULL_TIME)
-					System.out.println("Employee is Present ");
-				else
-					System.out.println("Employee is Absent");
-				
-		}
-		}
+
+	{
+			System.out.println("lets Calculate emp wage");
+			
+			int IS_FULL_TIME = 1;
+			int EMP_RATE_PER_HR = 20;
+					int empHrs = 0;
+					int empWage = 0;
+					
+			double empcheck = Math.floor(Math.random() * 10)% 2;
+			
+			if (empcheck == IS_FULL_TIME)
+				empHrs = 8;
+			
+			//	System.out.println("Employee is Present");
+			else
+				//System.out.println("Employee is Absent");
+				empHrs = 0;
+			empWage = empHrs * EMP_RATE_PER_HR ;
+			System.out.println("Emp Wage: "  + empWage);
+		
+	}
+	}
 
